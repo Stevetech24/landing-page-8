fd15167c61af184e6e1afaf5690d4eac3b29c204
Steve M <madubuikeebere203@gmail.com>
2025-11-27 13:15:17 +0100

    initial landing page 8 files

---
+++ script.js
@@ -0,0 +1,24 @@
+const sideResponsiveBar = document.getElementById("responsive-side-bar");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+
+openMenu.addEventListener("click", () => {
+  sideResponsiveBar.style.transform = " translateX(0)";
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+  document.body.classList.toggle("no-scroll");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 480) {
+    openMenu.style.display = "none";
+    closeMenu.style.display = "none";
+  }
+});
+
+closeMenu.addEventListener("click", () => {
+  sideResponsiveBar.style.transform = " translateX(-500px)";
+  closeMenu.style.display = "none";
+  openMenu.style.display = "block";
+  document.body.classList.remove("no-scroll");
+});
